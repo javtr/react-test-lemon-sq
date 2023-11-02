45878d0851d5378417cc0e1f4e164478e82d8b60
javtr <javtriana.z@gmail.com>
2023-11-02 12:57:24 -0500

    test links de pago

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,19 +8,17 @@ export default function Home() {
         <div className="home__card--cardButons">
           <div className="home__card--cardButons--buton">
             <h3>Product</h3>
-            <a href="https://www.youtube.com" target="_blank">
+            <a href="https://otmtrading.lemonsqueezy.com/checkout/buy/b3ba82d0-6bae-4528-b6c4-11b5b9732abc" target="_blank">
               Boton
             </a>
           </div>
           <div className="home__card--cardButons--buton">
             <h3>Product</h3>
-            <a href="https://www.youtube.com" target="_blank">
-              Boton
-            </a>
+            <a href="https://otmtrading.lemonsqueezy.com/checkout/buy/b3ba82d0-6bae-4528-b6c4-11b5b9732abc?embed=1" class="lemonsqueezy-button">Buy OTM Pack</a><script src="https://assets.lemonsqueezy.com/lemon.js" defer></script>
           </div>
           <div className="home__card--cardButons--buton">
             <h3>Product</h3>
-            <a href="https://www.youtube.com" target="_blank">
+            <a href="https://otmtrading.lemonsqueezy.com/checkout/buy/5bef45a3-6ee5-496d-8ea4-10d367e2535b" target="_blank">
               Boton
             </a>
           </div>
