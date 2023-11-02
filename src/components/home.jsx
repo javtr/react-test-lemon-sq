4bc07c9dd7fa510e6edece00434742519594fb33
javtr <javtriana.z@gmail.com>
2023-11-02 11:15:49 -0500

    cards

---
+++ src/components/home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home__card">
+        <h2>TITULO</h2>
+        <div className="home__card--cardButons">
+          <div className="home__card--cardButons--buton">
+            <h3>Product</h3>
+            <a href="https://www.youtube.com" target="_blank">
+              Boton
+            </a>
+          </div>
+          <div className="home__card--cardButons--buton">
+            <h3>Product</h3>
+            <a href="https://www.youtube.com" target="_blank">
+              Boton
+            </a>
+          </div>
+          <div className="home__card--cardButons--buton">
+            <h3>Product</h3>
+            <a href="https://www.youtube.com" target="_blank">
+              Boton
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
